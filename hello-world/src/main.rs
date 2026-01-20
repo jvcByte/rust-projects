6c74c980641624436a19a5ec321e156aadb89cb0
jvcbyte <jvc8463@gmail.com>
2026-01-20 15:15:06 +0100

    refactor: update dependencies in Cargo.toml
    - regenerate Cargo.lock file due to updated dependencies
    - update actix-web, actix-http, and actix-codec versions to latest stable releases
    - remove unnecessary dependencies from actix-server and actix-utils packages
    - add missing dependencies for actix-macros and actix-router packages
    
    ---
    Files changed:
    A	.gitignore
    A	hello-world/Cargo.lock
    M	hello-world/Cargo.toml
    M	hello-world/src/main.rs

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get, middleware::Logger, post, web};
+
+struct AppState {
+    app_name: String,
+}
+
+#[get("/")]
+async fn hello(data: web::Data<AppState>) -> String {
+    let app_name = data.app_name.clone();
+    format!("Hello {app_name}!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let env = env_logger::Env::default().filter_or("RUST_LOG", "debug");
+    env_logger::Builder::from_env(env).init();
+
+    HttpServer::new(move || {
+        let logger = Logger::default();
+        App::new()
+            .wrap(logger)
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
